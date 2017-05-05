d955c3551061a6baa93ec97698e1d8ba99d3290e
wanghuaying <wang13667539325@163.com>
2017-05-05 14:40:53 +0800

    api目录提交

---
+++ src/main/java/com/hshc/api/erp/FenxiaoDistributorProductsGet.java
@@ -0,0 +1,7 @@
+package com.hshc.api.erp;
+
+/**
+ * Created by wanghuaying on 2017/5/5.
+ */
+public class FenxiaoDistributorProductsGet {
+}
---
+++ src/main/java/com/hshc/api/tmall/FenxiaoDistributorProductsGet.java
@@ -0,0 +1,7 @@
+package com.hshc.api.tmall;
+
+/**
+ * Created by wanghuaying on 2017/5/5.
+ */
+public class FenxiaoDistributorProductsGet {
+}
