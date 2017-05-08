bee109c6efd0814287d1f141908f9d66c308a348
wanghuaying <wang13667539325@163.com>
2017-05-08 17:55:13 +0800

    Merge branch 'master' of https://github.com/zinave/leasing-tmall
    # Conflicts:
    #	pom.xml
    发布后端商品

---
+++ src/main/java/com/hshc/relay/dao/ScitemMapAddRequestDao.java
@@ -0,0 +1,11 @@
+package com.hshc.relay.dao;
+
+import com.taobao.api.request.ScitemMapAddRequest;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by wanghuaying on 2017/5/8.
+ */
+@Repository
+public interface ScitemMapAddRequestDao extends BaseDao<ScitemMapAddRequest> {
+}
