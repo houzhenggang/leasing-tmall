b203d16c665c90d4580bc1a89af29a014c08433d
wuguowei1314 <wuguowei_jing@163.com>
2017-05-09 10:30:55 +0800

    taobao.inventory.query (查询商品库存信息)

---
+++ src/main/java/com/hshc/relay/service/InventoryQueryService.java
@@ -0,0 +1,32 @@
+package com.hshc.relay.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taobao.api.ApiException;
+import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.TaobaoClient;
+import com.taobao.api.request.InventoryQueryRequest;
+import com.taobao.api.request.ItemAddRequest;
+import com.taobao.api.response.InventoryQueryResponse;
+import com.taobao.api.response.ItemAddResponse;
+
+/**
+ * taobao.inventory.query (查询商品库存信息)
+ * @author 吴国伟
+ *
+ */
+@Service
+public class InventoryQueryService extends BaseService<InventoryQueryRequest> {
+	@Autowired
+	private AuthorizedSessionService authorizedSessionService;
+	
+	public InventoryQueryResponse inventoryQuery(InventoryQueryRequest inventoryQueryRequest) throws ApiException {
+
+        TaobaoClient client = new DefaultTaobaoClient(inventoryQueryRequest.getApiMethodName(), authorizedSessionService.getAppKey(), authorizedSessionService.getAppSecret());
+
+        return client.execute(inventoryQueryRequest, authorizedSessionService.getAuthorizedSession("sandbox_taobao1234").getAccessToken());
+
+    }
+
+}
---
+++ src/test/java/com/hshc/relay/service/InventoryQueryServiceTest.java
@@ -0,0 +1,32 @@
+package com.hshc.relay.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hshc.relay.BaseTest;
+import com.taobao.api.ApiException;
+import com.taobao.api.request.InventoryQueryRequest;
+import com.taobao.api.response.InventoryQueryResponse;
+
+/**
+ * taobao.inventory.query (查询商品库存信息)
+ * @author 吴国伟
+ *
+ */
+public class InventoryQueryServiceTest extends BaseTest{
+
+	@Autowired
+	private InventoryQueryService inventoryQueryService;
+	
+	@Test
+	public void inventoryQueryTest() throws ApiException{
+		InventoryQueryRequest req = new InventoryQueryRequest();
+		req.setScItemIds("550354838075");
+		//req.setScItemCodes("GLY201210120001^GLY23214141");
+		//req.setSellerNick("Nike");
+		//req.setStoreCodes("GLY001^GLY002");
+		InventoryQueryResponse rsp = inventoryQueryService.inventoryQuery(req);
+		Assert.assertNotNull(rsp);
+	}
+}
