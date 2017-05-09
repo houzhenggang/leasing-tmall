e7d26f6cc504b21e0eb5fcac81a1f520d958bab8
sk <sk@sk-PC>
2017-05-09 10:39:08 +0800

    BaseTest

--- src/main/java/com/taobao/api/request/TmallCarLeaseReserveRequest.java
+++ src/main/java/com/taobao/api/request/TmallCarLeaseReserveRequest.java
@@ -3,6 +3,10 @@ package com.taobao.api.request;
 import com.taobao.api.internal.util.RequestCheckUtils;
 import java.util.Map;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 import com.taobao.api.ApiRuleException;
 import com.taobao.api.BaseTaobaoRequest;
 import com.taobao.api.internal.util.TaobaoHashMap;
@@ -27,26 +31,31 @@ public class TmallCarLeaseReserveRequest extends BaseTaobaoRequest<TmallCarLease
 	/** 
 	* 买家昵称
 	 */
+	@NotBlank(message = "买家昵称不能为空")
 	private String buyerNick;
 
 	/** 
 	* 1 代表 车辆到店，已预约用户到店提车   ; 2 车辆到店，未能联系到用户
 	 */
+	@NotNull(message = "车辆状态flag不能为空")
 	private Long flag;
 
 	/** 
 	* 订单id
 	 */
+	@NotNull(message = "订单id不能为空")
 	private Long orderId;
 
 	/** 
 	* 文案
 	 */
+	@NotBlank(message = "文案不能为空")
 	private String text;
 
 	/** 
 	* 车架号
 	 */
+	@NotBlank(message = "车架号不能为空")
 	private String vin;
 
 	public void setBuyerId(Long buyerId) {
