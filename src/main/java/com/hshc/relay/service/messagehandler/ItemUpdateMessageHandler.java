ee9c3144fa76f5cbb9df1d8fcc6443e5e55b31a5
zinave <36947601@qq.com>
2017-05-10 16:53:15 +0800

    MessageService完善处理消息的逻辑； 配置增加回传租赁的奇门地址；

---
+++ src/main/java/com/hshc/relay/service/messagehandler/ItemUpdateMessageHandler.java
@@ -0,0 +1,24 @@
+package com.hshc.relay.service.messagehandler;
+
+import com.hshc.relay.entity.User;
+import com.hshc.relay.service.BaseService;
+import com.taobao.api.ApiException;
+import com.taobao.api.internal.tmc.Message;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+
+/**
+ * @author 钟林俊
+ * @version V1.0 2017-05-10 13:38
+ */
+@Service
+public class ItemUpdateMessageHandler extends BaseService implements HshcMessageHandler {
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void handle(Message message) throws ApiException, IOException {
+    }
+
+}
