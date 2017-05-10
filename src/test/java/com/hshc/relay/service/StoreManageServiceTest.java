1f93191991bae2814eba6a320f29fd2cc29474cb
sk <sk@sk-PC>
2017-05-10 16:25:38 +0800

    添加注释

--- src/test/java/com/hshc/relay/service/StoreManageServiceTest.java
+++ src/test/java/com/hshc/relay/service/StoreManageServiceTest.java
@@ -3,11 +3,14 @@ package com.hshc.relay.service;
 import java.util.List;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.Feature;
 import com.hshc.relay.BaseTest;
+import com.hshc.relay.controller.erp.StoreManageController;
 import com.hshc.relay.service.RequestTaobaoClientService;
 import com.hshc.relay.service.StoreManageService;
 import com.taobao.api.ApiException;
@@ -15,38 +18,41 @@ import com.taobao.api.DefaultTaobaoClient;
 import com.taobao.api.TaobaoClient;
 import com.taobao.api.domain.Store;
 import com.taobao.api.request.InventoryStoreManageRequest;
+import com.taobao.api.request.TmallCarLeaseConsumeRequest;
+import com.taobao.api.request.TmallCarLeaseConsumeRequest.CosumeCodeReqDto;
 import com.taobao.api.request.TmallCarLeaseReserveRequest;
+import com.taobao.api.request.TmallCarLeaseTailpaymentbackRequest.TailPaymentDto;
 import com.taobao.api.response.InventoryStoreManageResponse;
+import com.taobao.api.response.TmallCarLeaseConsumeResponse;
 import com.taobao.api.response.TmallCarLeaseReserveResponse;
+import com.taobao.api.response.TmallCarLeaseTailpaymentbackResponse;
 import com.taobao.api.response.TmallCarLeaseReserveResponse.Result;
 
 public class StoreManageServiceTest extends BaseTest{
 
+	public static final Logger LOGGER = LoggerFactory.getLogger(StoreManageController.class); 
+	
 	@Autowired
-	private CarLeaseReserveService clrService;
+	CarLeaseConsumeService clcService;
 	@Autowired
-	private AuthorizedSessionService asService;
+	private CarLeaseTailpaymentbackService cltService;
 	
 	@Test
 	public void testStoreManage(){
-		TmallCarLeaseReserveRequest req = new TmallCarLeaseReserveRequest();
-		req.setBuyerId(123211313L);
-		req.setOrderId(1321321321L);
-		req.setText("车辆已到店，已预约消费者2015-05-12到店提车");
-		req.setVin("AFafsadf");
-		req.setFlag(1L);
-		req.setBuyerNick("张三");
+		TailPaymentDto obj1 = new TailPaymentDto();
+		obj1.setBuyerId(1312313L);
+		obj1.setMonthlyPay(2000L);
+		obj1.setMonths(12L);
+		obj1.setName("分期付尾款");
+		obj1.setOrderId(13131232132L);
+		obj1.setTailAmount(80000L);
 		try {
-			TaobaoClient client = new DefaultTaobaoClient("http://gw.api.taobao.com/router/rest", asService.getAppKey(), asService.getAppSecret());
-			TmallCarLeaseReserveResponse rsp = client.execute(req, asService.getAuthorizedSession("花生好车旗舰店").getAccessToken());
-			System.out.println("==========="+rsp.getBody());
-			//res.getBody()返回值 json
-			//结果存储对应表
-			//StoreManage storeManage = (StoreManage) JSON.(rsp.getBody());
-			clrService.addLeaseReserve(rsp);
-		} catch (ApiException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//获取返回值
+			TmallCarLeaseTailpaymentbackResponse leaseTailpaymentback = cltService.leaseTailpaymentback(obj1);
+			//保存返回的信息
+			cltService.addleaseTailpaymentback(leaseTailpaymentback.getResult());
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
 	}
 }
