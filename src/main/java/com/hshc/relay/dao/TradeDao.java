628d71bfc25612cb0d74a20795fc1d625a2acca4
sk <sk@sk-PC>
2017-05-11 17:08:42 +0800

    获取单笔交易的详细信息

---
+++ src/main/java/com/hshc/relay/dao/TradeDao.java
@@ -0,0 +1,14 @@
+package com.hshc.relay.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.taobao.api.domain.Trade;
+/**
+ * 主订单
+ * @author 史珂
+ * 2017年5月11日下午2:18:45
+ */
+@Repository
+public interface TradeDao extends BaseDao<Trade>{
+
+}
