7cf72732ee58945481d368d8ba4d8adc09d9612f
wuguowei1314 <wuguowei_jing@163.com>
2017-05-11 21:38:02 +0800

    仓库

--- src/main/java/com/hshc/relay/controller/erp/InventoryMerchantAdjustController.java
+++ src/main/java/com/hshc/relay/controller/erp/InventoryMerchantAdjustController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
+import com.hshc.relay.annotation.QimenSignAuthentication;
 import com.hshc.relay.controller.BaseController;
 import com.hshc.relay.service.InventoryMerchantAdjustService;
 import com.hshc.relay.vo.BaseQimenResponseVo;
@@ -36,6 +37,7 @@ public class InventoryMerchantAdjustController extends BaseController{
 	
 	@RequestMapping("/merchant-adjust")
 	@ResponseBody
+	@QimenSignAuthentication
 	public BaseQimenResponseVo inventoryMerchantAdjust(InventoryMerchantAdjustRequest inventoryMerchantAdjustRequest){
 		//获取JSON
 //		System.out.println("res========"+res);
