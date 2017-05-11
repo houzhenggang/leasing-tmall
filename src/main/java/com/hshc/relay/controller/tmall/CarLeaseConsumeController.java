a9e4245e7ff30db0be7af5250fb605bf8fdc3da4
wuguowei1314 <wuguowei_jing@163.com>
2017-05-11 17:00:27 +0800

    整车租车回传预约信息

--- src/main/java/com/hshc/relay/controller/tmall/CarLeaseConsumeController.java
+++ src/main/java/com/hshc/relay/controller/tmall/CarLeaseConsumeController.java
@@ -6,15 +6,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.hshc.relay.annotation.QimenSignAuthentication;
 import com.hshc.relay.controller.BaseController;
 import com.hshc.relay.controller.erp.StoreManageController;
 import com.hshc.relay.service.CarLeaseConsumeService;
 import com.hshc.relay.vo.BaseQimenResponseVo;
 import com.taobao.api.request.TmallCarLeaseConsumeRequest.CosumeCodeReqDto;
+import com.taobao.api.request.TmallCarLeaseTailpaymentbackRequest.TailPaymentDto;
 import com.taobao.api.response.TmallCarLeaseConsumeResponse;
 /**
  * 汽车租赁核销
@@ -33,8 +36,9 @@ public class CarLeaseConsumeController extends BaseController {
 	@RequestMapping("/lease-consume")
 	@ResponseBody
 	@QimenSignAuthentication
-	public BaseQimenResponseVo leaseConsume(@Valid CosumeCodeReqDto cosumeCodeReqDto){
+	public BaseQimenResponseVo leaseConsume(@RequestBody String res){
 		try {
+			CosumeCodeReqDto cosumeCodeReqDto = JSON.parseObject(res,CosumeCodeReqDto.class);
 			//获取返回参数
 			TmallCarLeaseConsumeResponse leaseConsume = clcService.leaseConsume(cosumeCodeReqDto);
 			LOGGER.info("leaseConsume:"+leaseConsume.getBody());
