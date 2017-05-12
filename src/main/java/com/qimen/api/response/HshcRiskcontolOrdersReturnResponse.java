5f6145e72366fe5832e0cc826b0ddfde1e3ea9cc
wuguowei1314 <wuguowei_jing@163.com>
2017-05-12 10:36:20 +0800

    奇门到erp

---
+++ src/main/java/com/qimen/api/response/HshcRiskcontolOrdersReturnResponse.java
@@ -0,0 +1,46 @@
+package com.qimen.api.response;
+
+import com.taobao.api.internal.mapping.ApiField;
+
+import com.qimencloud.api.AbstractQimenCloudResponse;
+
+/**
+ * TOP API(QimenCloud): hshc.riskcontol.orders.return response.
+ * 
+ * @author top auto create
+ * @since 1.0, null
+ */
+public class HshcRiskcontolOrdersReturnResponse extends AbstractQimenCloudResponse {
+
+	private static final long serialVersionUID = 2831222423532873447L;
+
+	/** 
+	 * 一级响应参数
+	 */
+	@ApiField("message")
+	private String message;
+
+	/** 
+	 * 一级响应消息
+	 */
+	@ApiField("success")
+	private Boolean success;
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getMessage( ) {
+		return this.message;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+	public Boolean getSuccess( ) {
+		return this.success;
+	}
+	
+
+
+}
