f71c7a4b94fc199294580723e0904c7112240a1c
wuguowei1314 <wuguowei_jing@163.com>
2017-05-13 14:21:43 +0800

    核销处理

--- src/main/java/com/hshc/relay/controller/tmall/CarLeaseTailpaymentbackController.java
+++ src/main/java/com/hshc/relay/controller/tmall/CarLeaseTailpaymentbackController.java
@@ -4,18 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
 import com.hshc.relay.annotation.QimenSignAuthentication;
 import com.hshc.relay.controller.BaseController;
 import com.hshc.relay.controller.erp.StoreManageController;
 import com.hshc.relay.service.CarLeaseTailpaymentbackService;
 import com.hshc.relay.vo.BaseQimenResponseVo;
 import com.taobao.api.request.TmallCarLeaseTailpaymentbackRequest;
-import com.taobao.api.request.TmallCarLeaseTailpaymentbackRequest.TailPaymentDto;
 import com.taobao.api.response.TmallCarLeaseTailpaymentbackResponse;
 /**
  * 尾款处置方案回传
@@ -34,15 +31,16 @@ public class CarLeaseTailpaymentbackController extends BaseController {
 	@ResponseBody
 	@QimenSignAuthentication
 	public BaseQimenResponseVo leaseTailpaymentback(TmallCarLeaseTailpaymentbackRequest req){
+		TmallCarLeaseTailpaymentbackResponse rep = new TmallCarLeaseTailpaymentbackResponse();
 		try {
 			//获取返回值
-			TmallCarLeaseTailpaymentbackResponse leaseTailpaymentback = cltService.leaseTailpaymentback(req);
-			LOGGER.info("leaseTailpaymentback:"+leaseTailpaymentback.getBody());
+			rep = cltService.leaseTailpaymentback(req);
+			LOGGER.info("leaseTailpaymentback:"+rep.getBody());
 			//保存返回的信息
-			cltService.addleaseTailpaymentback(leaseTailpaymentback.getResult());
+			cltService.addleaseTailpaymentback(rep.getResult());
 		} catch (Exception e) {
-			// TODO: handle exception
+			return new BaseQimenResponseVo(rep.getResult().getSuccess(),rep.getResult().getMsgInfo());
 		}
-		return new BaseQimenResponseVo("尾款处置方案回传成功");
+		return new BaseQimenResponseVo(rep.getResult().getSuccess(),rep.getResult().getMsgInfo());
 	}
 }
