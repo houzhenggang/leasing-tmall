d93d61754fd4dc771c58bebfc4eb33a7dd97d0a0
wanghuaying <wang13667539325@163.com>
2017-05-13 13:24:03 +0800

    同步租赁方案信息加验签

---
+++ src/main/java/com/hshc/relay/dao/UserSellerGetDao.java
@@ -0,0 +1,11 @@
+package com.hshc.relay.dao;
+
+import com.taobao.api.response.UserSellerGetResponse;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by wanghuaying on 2017/5/12.
+ */
+@Repository
+public interface UserSellerGetDao extends BaseDao<UserSellerGetResponse> {
+}
