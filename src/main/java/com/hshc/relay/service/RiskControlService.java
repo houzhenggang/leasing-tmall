50b054b3f63bc831bed5222a8ac47d9aac6e9226
zinave <36947601@qq.com>
2017-05-17 16:50:52 +0800

    风控业务增加注释

--- src/main/java/com/hshc/relay/service/RiskControlService.java
+++ src/main/java/com/hshc/relay/service/RiskControlService.java
@@ -32,16 +32,25 @@ public class RiskControlService extends BaseService<Customer> {
     @Autowired
     private CustomerDao customerDao;
 
+    /**
+     * 增加客户申请信息
+     *
+     * @param customer 客户申请信息
+     * @return 数据库新增影响的行数
+     */
     @Transactional(rollbackFor = Exception.class)
     public int add(final Customer customer){
+        // 客户对象不能为空
         Preconditions.checkNotNull(customer, "accepted customer is null");
+        // debug日志
         logger.debug("risk-control customer :" + JSON.toJSONString(customer));
+        // 持久化
         int rows = baseDao.insert(customer);
 
+        // 事务提交后执行 将客户信息发送给租赁系统
         TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
             @Override
             public void afterCommit() {
-
                 QimenCloudClient cloudClient = new DefaultQimenCloudClient(getReturnUrl(), getAppKey(), getAppSecret());
                 HshcRiskControlCustomerReturnRequest request = new HshcRiskControlCustomerReturnRequest();
                 request.setConsigneeAddress(customer.getAddr());
@@ -53,6 +62,7 @@ public class RiskControlService extends BaseService<Customer> {
 
                 try {
                     HshcRiskControlCustomerReturnResponse response = cloudClient.execute(request);
+                    // 发送成功后更新成功发送的标记
                     if(response.getSuccess() != null && response.getSuccess()){
                         customer.setReturned(true);
                         modify(customer);
@@ -67,18 +77,31 @@ public class RiskControlService extends BaseService<Customer> {
 
     }
 
+    /**
+     * 风控结果发送天猫
+     *
+     * @param uuid 客户申请号
+     * @return 天猫响应结果
+     * @throws ApiException
+     */
     public TmallCarLeaseRiskcallbackResponse.Result sendRiskControlResult(String uuid) throws ApiException {
+        // uuid必须有值
         Preconditions.checkArgument(!Strings.isNullOrEmpty(uuid), "the given uuid is null");
         Customer customer = new Customer();
         customer.setUuid(uuid);
+        // 根据uuid查询对应的客户信息
         TmallCarLeaseRiskcallbackRequest.CreditInfoTopDto topDto = customerDao.selectTopDto(customer);
+        // 没有相应客户抛异常退出
         Preconditions.checkNotNull(topDto, "no customer matches the given uuid " + uuid);
+        // 客户风控默认通过
         topDto.setPass(true);
 
+        // 发送天猫
         TaobaoClient client = new DefaultTaobaoClient(getTopApi(), getAppKey(), getAppSecret());
         TmallCarLeaseRiskcallbackRequest req = new TmallCarLeaseRiskcallbackRequest();
         req.setCreditInfo(topDto);
         TmallCarLeaseRiskcallbackResponse.Result result = client.execute(req, authorizedSessionService.getAuthorizedSession("sandbox_taobao1234").getAccessToken()).getResult();
+        // 回调日志记录
         logger.info("risk control callback : request=" + JSON.toJSONString(topDto) + ", resposne=" + JSON.toJSONString(result));
         return result;
     }
