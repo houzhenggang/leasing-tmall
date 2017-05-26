d21f4c5f502c891703b2521769098fe5e5bf342d
wuguowei1314 <wuguowei_jing@163.com>
2017-05-26 12:11:08 +0800

    门店仓库关系

--- src/main/java/com/hshc/relay/controller/tmall/LocationRelationEditController.java
+++ src/main/java/com/hshc/relay/controller/tmall/LocationRelationEditController.java
@@ -3,32 +3,41 @@ package com.hshc.relay.controller.tmall;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.hshc.relay.controller.BaseController;
+import com.hshc.relay.controller.erp.ScitemController;
 import com.hshc.relay.service.LocationRelationEditService;
+import com.hshc.relay.vo.BaseQimenResponseVo;
 import com.taobao.api.ApiException;
 import com.taobao.api.request.LocationRelationEditRequest;
+import com.taobao.api.response.LocationRelationEditResponse;
 
 
 @Controller
 public class LocationRelationEditController extends BaseController{
+    private static Logger LOGGER = LoggerFactory.getLogger(ScitemController.class);
 
 	@Autowired
 	private LocationRelationEditService locationRelationEditService;
 
 	@RequestMapping(value="/realtion-edit")
 	@ResponseBody
-	public Map<String,Object> locationRelationEdit()throws ApiException{
-		Map<String,Object> map = new HashMap<String,Object>();
-		
-		locationRelationEditService.locationRelationEdit(new LocationRelationEditRequest());
-		return map;
+	public BaseQimenResponseVo locationRelationEdit()throws ApiException{
+		LocationRelationEditResponse rsp =  new LocationRelationEditResponse();
+		try{
+	    rsp = locationRelationEditService.locationRelationEdit();
+		LOGGER.info("getBody:"+JSON.toJSONString(rsp));
+	} catch (Exception e) {
+		new BaseQimenResponseVo(false,JSON.toJSONString(rsp));
+	}
+	return new BaseQimenResponseVo(true,JSON.toJSONString(rsp));
 	}
 	
 }
