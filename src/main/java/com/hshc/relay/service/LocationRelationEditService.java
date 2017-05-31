35d07f9fe1cae3fc28a6782c0910a427b7740838
zinave <36947601@qq.com>
2017-05-31 16:49:09 +0800

    调整代码规范，优化部分逻辑，删除多余文件，增加未完成页面

--- src/main/java/com/hshc/relay/service/LocationRelationEditService.java
+++ src/main/java/com/hshc/relay/service/LocationRelationEditService.java
@@ -3,13 +3,10 @@ package com.hshc.relay.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
-import com.hshc.relay.controller.erp.ScitemController;
 import com.hshc.relay.dao.LocationRealtionEditDao;
 import com.taobao.api.ApiException;
 import com.taobao.api.DefaultTaobaoClient;
